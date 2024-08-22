d987398d3b80a6e9cf5660f71a1ffb3e44bcf6f6
norikawasonoka <sojgtfk@gmail.com>
2024-08-22 17:35:50 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,25 +4,20 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
-}
-= {
-  //...
+  ],
 
   // add daisyUI plugin
   plugins: [require("daisyui")],
 
   // daisyUI config (optional - here are the default values)
   daisyui: {
-    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "retro", // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
+    themes: false,
+    darkTheme: "retro",
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: "",
+    logs: true,
+    themeRoot: ":root",
   },
-
-  //...
 }
