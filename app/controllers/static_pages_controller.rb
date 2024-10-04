df1169493be196c82f0358c552d1eff26dedcdd6
norikawasonoka <sojgtfk@gmail.com>
2024-10-04 15:41:53 +0900

    トップ

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,11 @@
 
 # This controller handles static pages.
 class StaticPagesController < ApplicationController
+  skip_before_action :require_login, only: [:top, :terms]
+  
   def top; end
 
   def terms; end
+
+  def after_login; end
 end
