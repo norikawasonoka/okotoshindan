55ed8fcbc04f8b263682c0ab0a42b2fab654ae17
norikawasonoka <sojgtfk@gmail.com>
2024-10-15 19:36:18 +0900

    友達ボタン

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// ここから新しいコードを追加 他のページからトップページに戻ってもラインのボタンが再生成
+document.addEventListener("turbo:load", function() {
+    var buttonContainer = document.getElementById('line-button-container');
+    if (buttonContainer) {
+      buttonContainer.innerHTML = '<div class="line-it-button" data-lang="ja" data-type="friend" data-env="REAL" data-lineId="@501hrlmq" style="display: block;"></div>';
+      // LINEボタンの再生成
+      if (typeof LineIt !== 'undefined') {
+        LineIt.loadButton();
+      }
+    }
+  });
+  
