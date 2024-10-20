1f7cccb1443f0ea4b05893f80b2226b2bc4ef4da
norikawasonoka <sojgtfk@gmail.com>
2024-10-20 20:40:27 +0900

    ユーチューブとつなげ役

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,10 +1,6 @@
 class Video < ApplicationRecord
-  #YoutubeVideoがActiveHash::BaseのためApplicationRecordがないとつながらない
+  #YoutubeVideoがActiveHash::BaseのためApplicationRecordがないとつながらないためyoutubeとつなげるvideoを作成
   belongs_to :result
   has_many :users, through: :favorites
   has_many :favorites, dependent: :destroy
-
-  #def youtube_video
-    #YoutubeVideo.find_by(id: self.youtube_video_id)
-  #end
 end
