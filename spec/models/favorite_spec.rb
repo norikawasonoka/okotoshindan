96009496656c1e959051ac45d3762cdf4e99f07c
norikawasonoka <sojgtfk@gmail.com>
2024-11-08 12:43:59 +0900

    テスト

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Favorite, type: :model do
     let(:user) { create(:user) }
     let(:video) { create(:video) }
     let(:result) { create(:result) }  # resultを追加
-    let(:favorite) { create(:favorite, user: user, video: video, result: result) }
+    let(:favorite) { Favorite.new(user: user, video: video, result: result) }
 
     it 'is valid with a user, a video, and a result' do
       favorite = Favorite.new(user: user, video: video, result: result)  # resultを設定
