87f832938d0cdf27de0dc8cc85dd24044e115ba1
norikawasonoka <sojgtfk@gmail.com>
2024-11-11 19:30:35 +0900

    テスト

--- spec/requests/line_login_api_spec.rb
+++ spec/requests/line_login_api_spec.rb
@@ -3,7 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe 'LineLoginApis', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /login' do
+    it 'redirects to the LINE authorization URL' do
+      get line_login_api_login_path # 適切なパスを指定するダ
+      expect(response).to redirect_to(/access.line.me/)
+      expect(session[:state]).to be_present
+    end
   end
 end
