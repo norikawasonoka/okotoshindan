1b135b5bee71d1544f061eef814d94ed1fcfc3cc
norikawasonoka <sojgtfk@gmail.com>
2024-12-12 17:19:01 +0900

    追加したビデオのみseedに追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,16 +17,16 @@ results = Result.all # 全ての結果を取得
 # YoutubeVideoがActiveHashを使用しているためseedを使用して取得
 
 # 全ての YoutubeVideo データを取得して Video レコードを更新または作成
+#find_or_create_by! でデータを登録→
+#Result や Video モデルに対して、同じデータが既に存在している場合はスキップし、存在しない場合のみデータを作成します。
 YoutubeVideo.all.each do |youtube_video|
   result = Result.find_by(id: youtube_video.result_id)
   next unless result
 
-  Video.create!(
+  Video.find_or_create_by!(
     title: youtube_video.title,
     embed_code: youtube_video.embed_code,
     youtube_video_id: youtube_video.id,
     result_id: youtube_video.result_id
   )
 end
-
-puts 'YouTube video data has been updated or created successfully.'
